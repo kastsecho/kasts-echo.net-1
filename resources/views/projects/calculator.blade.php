05bf229e1f004d82e95b8cbc586f47c100a102a8
Jared Lewis <jslewis90@outlook.com>
2022-05-27 09:47:42 -0400

    wip

--- resources/views/projects/calculator.blade.php
+++ resources/views/projects/calculator.blade.php
@@ -1,53 +1,67 @@
-<div id="calc" v-cloak>
-    <div>
-        <label for="length">Length (inches)</label>
-        <input id="length" type="number" v-model="length" min="1">
-    </div>
-    <div>
-        <label for="width">Width (inches)</label>
-        <input id="width" type="number" v-model="width" min="1">
-    </div>
-    <div>
-        <label for="cost">Cost / inch:</label>
-        <strong v-text="costPerInch"></strong>
-    </div>
-    <div>
-        Estimated cost:
-        <span v-text="estimate"></span>
-    </div>
-</div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script src="https://unpkg.com/vue@3"></script>
-<script defer>
-    let { createApp } = Vue;
+    <title>Calculator</title>
 
-    createApp({
-        data() {
-            return {
-                length: 1,
-                width: 1,
-                cost: 0.0175,
-            }
-        },
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="min-vh-100 d-grid align-items-center justify-content-center">
+    <div id="calc" v-cloak>
+        <div class="mb-3">
+            <label for="length">Length (inches)</label>
+            <input class="form-control" id="length" type="number" v-model="length" min="1">
+        </div>
+        <div class="mb-3">
+            <label for="width">Width (inches)</label>
+            <input class="form-control" id="width" type="number" v-model="width" min="1">
+        </div>
+        <div class="mb-3 d-flex justify-content-between">
+            <span>Cost / inch:</span>
+            <strong v-text="costPerInch"></strong>
+        </div>
+        <div class="d-flex justify-content-between">
+            <span>Estimated cost:</span>
+            <strong v-text="estimate"></strong>
+        </div>
+    </div>
 
-        computed: {
-            estimate() {
-                return this.format(this.length * this.width * this.cost);
+    <script src="https://unpkg.com/vue@3"></script>
+    <script defer>
+        let { createApp } = Vue;
+
+        createApp({
+            data() {
+                return {
+                    length: 1,
+                    width: 1,
+                    cost: 0.0175,
+                }
             },
 
-            costPerInch() {
-                return this.format(this.cost);
-            }
-        },
+            computed: {
+                estimate() {
+                    return this.format(this.length * this.width * this.cost);
+                },
 
-        methods: {
-            format(cost, type = 'USD') {
-                return new Intl.NumberFormat('en-US', {
-                    style: 'currency',
-                    currency: type,
-                    minimumFractionDigits: 4,
-                }).format(cost);
+                costPerInch() {
+                    return this.format(this.cost);
+                }
+            },
+
+            methods: {
+                format(cost, type = 'USD') {
+                    return new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: type,
+                        minimumFractionDigits: 4,
+                    }).format(cost);
+                }
             }
-        }
-    }).mount('#calc');
-</script>
+        }).mount('#calc');
+    </script>
+</body>
+</html>
