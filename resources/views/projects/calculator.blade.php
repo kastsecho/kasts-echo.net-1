086d5c63775825aaaa3e66340c21168ba3eb1b6a
Jared Lewis <jslewis90@outlook.com>
2022-05-28 22:08:04 -0400

    wip

--- resources/views/projects/calculator.blade.php
+++ resources/views/projects/calculator.blade.php
@@ -9,26 +9,81 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-light h-100 d-grid align-items-center justify-content-center">
-    <div id="calc" v-cloak>
-        <div class="mb-3">
-            <label for="length">Length (inches)</label>
-            <input class="form-control" id="length" type="number" v-model="length" min="1">
-        </div>
-        <div class="mb-3">
-            <label for="width">Width (inches)</label>
-            <input class="form-control" id="width" type="number" v-model="width" min="1">
-        </div>
-        <div class="mb-3 d-flex justify-content-between">
-            <span>Cost / inch:</span>
-            <strong v-text="costPerInch"></strong>
-        </div>
-        <div class="d-flex justify-content-between">
-            <span>Estimated cost:</span>
-            <strong v-text="estimate"></strong>
+<body class="bg-light">
+    <div class="vh-100 d-grid align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-8 col-lg-4" x-data="calc">
+                    <div class="mb-3">
+                        <label for="length">Length (inches)</label>
+                        <input class="form-control" id="length" type="number" min="1" x-model="length">
+                    </div>
+                    <div class="mb-3">
+                        <label for="width">Width (inches)</label>
+                        <input class="form-control" id="width" type="number" min="1" x-model="width">
+                    </div>
+                    <div class="mb-3 d-flex justify-content-between">
+                        <span>Cost / inch:</span>
+                        <strong x-text="costPerInch"></strong>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <span>Estimated cost:</span>
+                        <strong x-text="estimate"></strong>
+                    </div>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-8 col-lg-4" id="calc">
+                    <div class="mb-3">
+                        <label for="length">Length (inches)</label>
+                        <input class="form-control" id="length" type="number" v-model="length" min="1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="width">Width (inches)</label>
+                        <input class="form-control" id="width" type="number" v-model="width" min="1">
+                    </div>
+                    <div class="mb-3 d-flex justify-content-between">
+                        <span>Cost / inch:</span>
+                        <strong v-text="costPerInch"></strong>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <span>Estimated cost:</span>
+                        <strong v-text="estimate"></strong>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
+    <script src="https://unpkg.com/alpinejs" async></script>
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('calc', () => ({
+                length: 1,
+                width: 1,
+                cost: 0.0175,
+
+                get costPerInch() {
+                    return this.formatCost(this.cost);
+                },
+
+                get estimate() {
+                    return this.formatCost(this.length * this.width * this.cost, 2);
+                },
+
+                formatCost(value, digits = 4) {
+                    return new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: 'USD',
+                        minimumFractionDigits: digits,
+                    }).format(value);
+                }
+            }))
+        })
+    </script>
     <script src="https://unpkg.com/vue@3"></script>
     <script defer>
         let { createApp } = Vue;
